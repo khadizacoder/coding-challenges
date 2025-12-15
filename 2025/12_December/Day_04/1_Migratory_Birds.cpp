7a2b7b59cbe71b175882ad9a42f92363b272814a
Khadiza Coder <freelancerkhadiza2@gmail.com>
2025-12-15 23:46:44 +0600

    mi

--- 2025/12_December/Day_04/1_Migratory_Birds.cpp
+++ 2025/12_December/Day_04/1_Migratory_Birds.cpp
@@ -7,6 +7,14 @@
 using namespace std;
 int main()
 {
-    
+    int N;
+    cin >> N;
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = N; j >= 1; j /= 2)
+        {
+            cout << i << " " << j << endl;
+        }
+    }
     return 0;
 }
